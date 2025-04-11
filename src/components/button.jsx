69e2a7a4a91ab0f0fc922db9fdfdf65772b1dc25
Rekhansika <rekhagoluguri02@gmail.com>
2025-04-11 13:47:25 +0530

    child

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,1 +1,12 @@
 // write button card here
+import React from 'react'
+
+const button = () => {
+  return (
+    <button>
+        View Product
+    </button>
+  )
+}
+
+export default button
--- src/components/productcard.jsx
+++ src/components/productcard.jsx
@@ -1,1 +1,22 @@
 // write product card here
+import React from 'react'
+import Button from './button';
+
+const productcard = () => {
+
+    const productImage = "https://freightershop.in/cdn/shop/files/25.webp?v=1720604921&width=1445";
+    const productName = "Breathing Teddy";
+    const productPrice = "$11" ;
+  return (
+    <div className='productcard'>
+        <img src={productImage} alt="Image" />
+        <p>{productName}</p>
+        <p>{productPrice}</p>
+        <Button/>
+      
+    </div>
+  )
+}
+
+export default productcard
+
